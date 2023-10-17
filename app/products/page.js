274bf221750a1488bb349ca804d412193deb8c03
Betty Sosterics <betty.sosterics@gmail.com>
2023-10-17 08:55:13 +0200

    Fix products page mapping code for pnpm build

--- app/products/page.js
+++ app/products/page.js
@@ -1,46 +1,43 @@
-// import Image from 'next/image';
-// import Link from 'next/link';
-// import { getProducts } from '../../database/products';
+import Image from 'next/image';
+import Link from 'next/link';
+import { getProducts } from '../../database/products';
 
-import ProductsGrid from './ProductsGrid';
+// import ProductsGrid from './ProductsGrid';
 
 export const metadata = {
   title: 'Products',
   description: 'SlowPack Products Page',
 };
 
-export default function ProductsPage() {
-  return (
-    <>
-      <h1>Choose your SlowPack!</h1>
-      <span>{ProductsGrid()}</span>
-    </>
-  );
-  // const products = await getProducts();
+export default async function ProductsPage() {
   // return (
   //   <>
-  //     <h1 data-test-id="products-link">🐢 The SlowPacks 🐢</h1>
-  //     {products.map((product) => {
-  //       return (
-  //         <span
-  //           className={styles.productsGrid}
-  //           key={`product-div-${product.id}`}
-  //         >
-  //           <Link
-  //             href={`/products/${product.id}`}
-  //             data-test-id={`product-${product.id}`}
-  //           >
-  //             {product.name} {product.description}
-  //             <Image
-  //               src={`/images/${product.imageName}.png`}
-  //               alt={product.name}
-  //               width={200}
-  //               height={200}
-  //             />
-  //           </Link>
-  //         </span>
-  //       );
-  //     })}
+  //     <h1>Choose your SlowPack!</h1>
+  //     <span>{ProductsGrid()}</span>
   //   </>
   // );
+  const products = await getProducts();
+  return (
+    <>
+      <h1 data-test-id="products-link">🐢 The SlowPacks 🐢</h1>
+      {products.map((product) => {
+        return (
+          <div key={`product-div-${product.id}`}>
+            <Link
+              href={`/products/${product.id}`}
+              data-test-id={`product-${product.id}`}
+            >
+              {product.name} {product.description}
+              <Image
+                src={`/images/${product.imageName}.png`}
+                alt={product.name}
+                width={200}
+                height={200}
+              />
+            </Link>
+          </div>
+        );
+      })}
+    </>
+  );
 }
